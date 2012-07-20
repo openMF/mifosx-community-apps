d1a40d32db94286925d281c67d02ed5f0b785b5e
Keith Woodlock <keithwoodlock@gmail.com>
2012-07-20 11:11:25 +0100

    minor changes to loan ui

--- IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.04.js
+++ IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.04.js
@@ -1562,12 +1562,19 @@ function jsViewsRegisterHelpers() {
 			
 			money: function(monetaryObj) {
 				
+				if (undefined == monetaryObj) {
+					return "";
+				}
 				Globalize.culture().numberFormat.currency.symbol = monetaryObj.displaySymbol;
 				
 				var digits = monetaryObj.digitsAfterDecimal.toFixed(0);
 				return Globalize.format(monetaryObj.amount, "n" + digits); 
 			},
 			moneyWithCurrency: function(monetaryObj) {
+				
+				if (undefined == monetaryObj) {
+					return "";
+				}
 				
 				Globalize.culture().numberFormat.currency.symbol = monetaryObj.displaySymbol;
 				
