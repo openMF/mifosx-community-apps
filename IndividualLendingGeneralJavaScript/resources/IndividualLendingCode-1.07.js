a94b9d1e7559dfdd819074c6848fc74aee1f6838
Keith Woodlock <keithwoodlock@gmail.com>
2012-08-13 20:19:11 +0100

    fix serialisation of checkboxes and update labels and validation messages.

--- IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.07.js
+++ IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.07.js
@@ -1550,35 +1550,71 @@ function showNotAvailableDialog(titleCode) {
 }
 	
 $.fn.serializeObject = function()
-	{
-	    var o = {};
-	    var a = this.serializeArray();
-	    $.each(a, function() {
-	    	if (this.name === 'notSelectedCurrencies' || this.name === 'notSelectedPermissions' || this.name === 'notSelectedRoles') {
-	    		// do not serialize
-	    	} else  {
-
-	        if (o[this.name] !== undefined) {
-	            if (!o[this.name].push) {
-	                o[this.name] = [o[this.name]];
-	            }
-	            o[this.name].push(this.value || '');
-	        } else {
-	        	
-	        	if (this.name === 'selectedItems' || this.name === 'notSelectedItems' || this.name === 'currencies' || this.name === 'permissions' || this.name === 'roles') {
-	        		o[this.name] = new Array();
-	        		o[this.name].push(this.value || '');
-	        	} else {
-	        		o[this.name] = this.value || '';	
-	        	}
-	        }
-		}
-	    });
-	    return o;
-	};
+{
+	var o = {};
+	var a = this.serializeArray({ checkboxesAsBools: true});
 	
+	$.each(a, function() {
+		
+		if (this.name === 'notSelectedCurrencies' || this.name === 'notSelectedPermissions' || this.name === 'notSelectedRoles') {
+			// do not serialize
+		} else  {
+		    if (o[this.name] !== undefined) {
+		        if (!o[this.name].push) {
+		            o[this.name] = [o[this.name]];
+		        }
+		        o[this.name].push(this.value || '');
+		    } else {
+		    	
+		    	if (this.name === 'selectedItems' || this.name === 'notSelectedItems' || this.name === 'currencies' || this.name === 'permissions' || this.name === 'roles') {
+		    		o[this.name] = new Array();
+		    		o[this.name].push(this.value || '');
+		    	} else {
+		    		o[this.name] = this.value || '';	
+		    	}
+		    }
+		}
+	});
+	
+	return o;
+};
 
+$.fn.serializeArray = function (options) {
+    var o = $.extend({
+        checkboxesAsBools: false
+    }, options || {});
 
+    var rselectTextarea = /select|textarea/i;
+    var rinput = /text|hidden|password|search/i;
+
+    return this.map(function () {
+        return this.elements ? $.makeArray(this.elements) : this;
+    })
+    .filter(function () {
+        return this.name && !this.disabled &&
+            (this.checked
+            || (o.checkboxesAsBools && this.type === 'checkbox')
+            || rselectTextarea.test(this.nodeName)
+            || rinput.test(this.type));
+    })
+        .map(function (i, elem) {
+            var val = $(this).val();
+            return val == null ?
+            null :
+            $.isArray(val) ?
+            $.map(val, function (val, i) {
+                return { name: elem.name, value: val };
+            }) :
+            {
+                name: elem.name,
+                value: (o.checkboxesAsBools && this.type === 'checkbox') ? 
+                    (this.checked ? 'true' : 'false') :
+                    val
+            };
+        }).get();
+};
+
+	
 function displayListTable(tableDiv) {
 
 	return  $("#" + tableDiv).dataTable( {
