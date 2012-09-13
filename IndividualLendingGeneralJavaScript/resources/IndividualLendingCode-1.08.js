f0f891f4cd1c63a4c2aa8b5b1dddb6933bd04c1e
John Woodlock <john.woodlock@gmail.com>
2012-09-13 21:57:16 +1000

    WIP globalising dates for data tables

--- IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.08.js
+++ IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.08.js
@@ -2483,12 +2483,11 @@ function jsViewsRegisterHelpers() {
 			    	  {
 				    	  var dateParts = localDateAsISOString.split("-")
 				    	  var year = dateParts[0];
-				    	  var month = parseInt(dateParts[1]) - 1; // month is zero indexed
+				    	  var month = dateParts[1] - 1; // month is zero indexed
 				    	  var day = dateParts[2];
 				    	  
 				    	  var d = new Date();
 				    	  d.setFullYear(year,month,day);
-				    	  
 				    	  return Globalize.format(d,"dd MMMM yyyy");
 			    	  } else return "";
 			      } catch(e) {
