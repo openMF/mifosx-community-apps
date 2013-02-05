c834b35d60a31ed6031150ee72885a6867586133
Keith Woodlock <keithwoodlock@gmail.com>
2013-02-05 11:07:19 +0000

    add tenant specific properties for datatables.

---
+++ IndividualLendingGeneralJavaScript/resources/tenantconfigs/latam.js
@@ -0,0 +1,3 @@
+var customisationName = "";
+//customisationName = '<script type="text/javascript" src="resources/tenantconfigs/customisations/GKRiskAnalysis.js"></script>';
+//document.write(customisationName);
