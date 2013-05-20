aa971d62aa0be2d85eac8afe2e3773873ce0a625
Keith Woodlock <keithwoodlock@gmail.com>
2013-05-20 12:33:20 +0100

    MIFOSX-363: remove background image to restore white background.

--- IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.1.0.RELEASE.js
+++ IndividualLendingGeneralJavaScript/resources/IndividualLendingCode-1.1.0.RELEASE.js
@@ -190,7 +190,7 @@ function showMainContainer(containerDivName, username) {
 
 	var htmlVar = '<div id="logowrapper">';
 	htmlVar += '	<span style="float: left">';
-	htmlVar += '		<img style="float:left; border: 0;" alt="" src="resources/mifosx.jpg"/>';
+	htmlVar += '		<img style="float:left; border: 0;" alt="" src="resources/mifos.jpg"/>';
 	htmlVar += '	</span>';
 	htmlVar += '</div>';
 	htmlVar += '<div id="navwrapper">';
